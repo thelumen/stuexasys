a7ad87c831a30a34325e3f80159f0cfa3f036439
yangxuyue <mylove1621@qq.com>
2017-11-01 21:30:10 +0800

    3

---
+++ src/main/java/sunday/pojo/manager/FileInfo.java
@@ -0,0 +1,52 @@
+package sunday.pojo.manager;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+/**
+ * Created by yang on 2017/10/8.
+ * At 21:06
+ */
+public final class FileInfo {
+    private String fileName;
+    private String path;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date nowDate;
+
+    @Override
+    public String toString() {
+        return "FileInfo{" +
+                "fileName='" + fileName + '\'' +
+                ", path='" + path + '\'' +
+                ", nowDate=" + nowDate +
+                '}';
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        //\\\\被java转换成\\，\\又被正则表达式转换成\。
+        //this.path = path.replaceAll("\\\\", "/");
+        this.path = path.replace("\\", "/");
+    }
+
+    public void setNowDate(long lastModified) {
+        this.nowDate = new Date(lastModified);
+    }
+
+    public Date getNowDate() {
+        return nowDate;
+    }
+
+}
